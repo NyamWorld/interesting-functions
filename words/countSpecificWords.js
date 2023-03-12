4ab48b27b6b9dcb1e875340ba32393817a27153f
NyamWorld <114996131+NyamWorld@users.noreply.github.com>
2023-03-12 16:08:33 +0900

    Create countSpecificWords.js

---
+++ words/countSpecificWords.js
@@ -0,0 +1,15 @@
+// 'str'에 해당하는 텍스트 안에 'word'에 해당하는 글자의 수를 찾아내는 코드
+
+function countSpecificWord(str, word) {
+  const specificWord = word;
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (specificWord.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// example: console.log(countSpecificWord('Hello World','HW'))
+// >> 2
