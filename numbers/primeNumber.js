757f75331cb4452b5bd0f5b970d774124a9cdb3e
NyamWorld <114996131+NyamWorld@users.noreply.github.com>
2023-03-23 21:36:03 +0900

    Update primeNumber.js

--- numbers/primeNumber.js
+++ numbers/primeNumber.js
@@ -1,1 +1,15 @@
+// 주어진 수가 소수인지 아닌지를 반환하는 함수
+
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 
